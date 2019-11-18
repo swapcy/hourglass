f22e7ef9cbfbe8bb1dd23812d434be94120ebe49
Swapnil <email2swap@gmail.com>
2019-11-18 12:30:32 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBA4y5Lm-oK0nAPk9Pdz9k6KT6fq9N4nGE",
-    authDomain: "life-time-list.firebaseapp.com",
-    databaseURL: "https://life-time-list.firebaseio.com",
+    apiKey: "APIKEY",
+    authDomain: "NAME",
+    databaseURL: "URL",
     projectId: "life-time-list",
-    storageBucket: "life-time-list.appspot.com",
-    messagingSenderId: "556548776200",
-    appId: "1:556548776200:web:292681a2fd9ca60a65ccbd",
-    measurementId: "G-0R3Y5W8F1B"
+    storageBucket: "DOMAIN",
+    messagingSenderId: "SOMETHING",
+    appId: "OKIDGOESHERE",
+    measurementId: "ID"
   }
 };
 
